610cdff3cb561e1d9749d6292aed7691fd4eeebb
Raphael <plassa_r@etna-alternance.net>
2023-11-12 23:12:12 +0100

    ✨ Add componant

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,11 @@
-import { StyleSheet } from "react-native";
-
 import EditScreenInfo from "../../components/EditScreenInfo";
 import { Text, View } from "../../components/Themed";
+import TopStats from "../../components/TopStats";
 
 export default function TabOneScreen() {
   return (
     <View>
+      <TopStats />
       <Text className="text-lg text-center">Good habits</Text>
       <View />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
---
+++ components/TopStats.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "./Themed";
+
+export default function TopStats() {
+  return (
+    <View>
+      <Text>50%</Text>
+      <Text>Vous y êtes presque</Text>
+      <Text>Streak : 5 jours d'affilés</Text>
+    </View>
+  );
+}
