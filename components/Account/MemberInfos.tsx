4a1474efb5f0aab8f16d84b1c8cc32548c25377b
raphaeltkorp <raphael@tkorp.com>
2024-08-25 22:42:51 +0200

    refactor: Update MemberInfos component to use local image as fallback for profile picture

--- components/Account/MemberInfos.tsx
+++ components/Account/MemberInfos.tsx
@@ -11,37 +11,38 @@ export default function MemberInfos({
 	auth: any;
 	theme: any;
 }) {
-	<View style={{ backgroundColor: theme.colors.background }} className="mb-4">
-		<View
-			className="mx-auto flex flex-row pt-6 justify-center w-11/12 items-center mb-12"
-			style={{ backgroundColor: theme.colors.background }}
-		>
-			<Image
-				source={
-					member.profilePicture
-						? { uri: member.profilePicture }
-						: require("../../assets/images/pfp.jpg")
-				}
-				className="rounded-full mx-auto mt-4"
-				style={{ width: 120, height: 120 }}
-			/>
-			<View
-				className="mx-auto flex flex-col gap-5 max-w-[50%]"
-				style={{ backgroundColor: theme.colors.background }}
-			>
-				<Text
-					className=" ml-6 mb-4 text-xl mt-3"
-					style={{ color: theme.colors.text }}
-				>
-					{member?.nom}
-				</Text>
-				<Text
-					className=" ml-6 mb-4 text-lg mt-3"
-					style={{ color: theme.colors.text }}
-				>
-					{auth.currentUser?.email}
-				</Text>
-			</View>
-		</View>
-	</View>;
+    return (
+					<View
+						style={{ backgroundColor: theme.colors.background }}
+						className="mb-4"
+					>
+						<View
+							className="mx-auto flex flex-row pt-6 justify-center w-11/12 items-center mb-12"
+							style={{ backgroundColor: theme.colors.background }}
+						>
+							<Image
+								source={require("../../assets/images/pfp.jpg")}
+								className="rounded-full mx-auto mt-4"
+								style={{ width: 120, height: 120 }}
+							/>
+							<View
+								className="mx-auto flex flex-col gap-5 max-w-[50%]"
+								style={{ backgroundColor: theme.colors.background }}
+							>
+								<Text
+									className=" ml-6 mb-4 text-xl mt-3"
+									style={{ color: theme.colors.text }}
+								>
+									{member?.nom}
+								</Text>
+								<Text
+									className=" ml-6 mb-4 text-lg mt-3"
+									style={{ color: theme.colors.text }}
+								>
+									{auth.currentUser?.email}
+								</Text>
+							</View>
+						</View>
+					</View>
+				);
 }
