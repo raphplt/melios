15336170699a42e55413250fe13faa664ebd525d
raphplt <raphael.plassart@gmail.com>
2024-12-31 00:00:01 +0100

    Bump version to 1.1.13 and update build number to 12; refactor Version component to use useTheme and useTranslation for improved theming and localization

--- components/Account/Version.tsx
+++ components/Account/Version.tsx
@@ -1,9 +1,10 @@
-import { ThemeContext } from "@context/ThemeContext";
-import { useContext } from "react";
+import { useTheme } from "@context/ThemeContext";
+import { useTranslation } from "react-i18next";
 import { View, Text } from "react-native";
 
 export default function Version() {
-	const { theme } = useContext(ThemeContext);
+	const { theme } = useTheme();
+	const { t } = useTranslation();
 
 	return (
 		<View
@@ -11,7 +12,7 @@ export default function Version() {
 			style={{ backgroundColor: theme.colors.background }}
 		>
 			<Text className=" text-center text-sm" style={{ color: theme.colors.text }}>
-				Melios v1.1.7 - © 2024 Melios. Tous droits réservés.
+				Melios v1.1.13 - © 2025 Melios. {t("all_rights_reserved")}
 			</Text>
 		</View>
 	);
