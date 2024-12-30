bb4cbe72b79819d4cfa1e1cf0f664cb9a4537254
raphplt <raphael.plassart@gmail.com>
2024-12-30 22:35:21 +0100

    Update ModalTutorial background color for improved visibility

--- components/Modals/ModalTutoriel.tsx
+++ components/Modals/ModalTutoriel.tsx
@@ -82,7 +82,7 @@ export default function ModalTutorial({
 							left: 0,
 							right: 0,
 							bottom: 0,
-							backgroundColor: "rgba(0,0,0,0.4)",
+							backgroundColor: "rgba(0,0,0,0.6)",
 						}}
 					/>
 				)}
