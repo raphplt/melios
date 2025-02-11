70729111b67939e701e41f900f2de15a3e8e4388
raphaeltkorp <raphael@tkorp.com>
2025-02-11 21:37:27 +0100

    Refactor babel.config.js to remove module-resolver plugin and update package.json dependencies

--- babel.config.js
+++ babel.config.js
@@ -5,26 +5,6 @@ module.exports = function (api) {
 			["babel-preset-expo", { jsxImportSource: "nativewind" }],
 			"nativewind/babel",
 		],
-		plugins: [
-			[
-				"module-resolver",
-				{
-					alias: {
-						"@firebase/auth": "./node_modules/@firebase/auth/dist/index.rn.d.ts",
-						"@db": "./db",
-						"@hooks": "./hooks",
-						"@utils": "./utils",
-						"@components": "./components",
-						"@context": "./context",
-						"@assets": "./assets",
-						"@type": "./type",
-						"@constants": "./constants",
-						"@locales": "./locales",
-					},
-				},
-			],
-			"react-native-reanimated/plugin",
-			"react-native-iconify/plugin",
-		],
+		plugins: ["react-native-iconify/plugin"],
 	};
 };
